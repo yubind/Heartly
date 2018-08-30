8da3b831647a8126b4217c0bb8315c785d87cf0d
Yubin Kim <yubind91@gmail.com>
2018-08-30 03:59:26 +0000

    add about page

--- public/js/main.js
+++ public/js/main.js
@@ -1,30 +1,49 @@
 $( document ).ready(function() {
-    
-$('.testimonial-carousel').slick({
-  centerMode: true,
-  centerPadding: '20px',
-  slidesToShow: 3,
-  autoplay: true,
-  autoplaySpeed: 6000,
-  arrows: false,
-  dots: true,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        centerMode: true,
-        centerPadding: '20px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        centerMode: true,
-        centerPadding: '20px',
-        slidesToShow: 1
-      }
+  $('body').css('display', 'block');  
+  
+  $(window).on('scroll', function() {
+    if($(this).scrollTop() > 100) {
+      $('.second-banner-content').addClass('animated fadeInUp slow');
+      $('.second-banner-img').addClass('animated fadeInUp slow');
     }
-  ]
-});
+  });
+  
+  $(window).on('scroll', function() {
+    if($(this).scrollTop() > 300) {
+      $('.third-banner-content').addClass('animated fadeIn slow');
+    }
+  });
+  
+
+
+    
+  $('.testimonial-carousel').slick({
+    centerMode: true,
+    centerPadding: '20px',
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    arrows: false,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          centerMode: true,
+          centerPadding: '20px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          centerMode: true,
+          centerPadding: '20px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 });
